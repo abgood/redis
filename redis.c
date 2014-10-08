98dd4478a5a2f250351c1141416f6005c85542ea
june <abgood@sina.com>
2014-10-08 18:19:52 +0800

    hello redis

---
+++ redis.c
@@ -0,0 +1,7 @@
+#include "redis.h"
+
+int main (int argc, char **argv) {
+
+    printf("redis done\n");
+    return 0;
+}
---
+++ redis.h
@@ -0,0 +1,11 @@
+#ifndef REDIS_H
+#define REDIS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#endif
